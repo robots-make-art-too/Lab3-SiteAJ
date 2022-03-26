70914f9365f96e3976fc8283bcdfcb0ac73e561c
ajkhaled <59937312+ajkhaled@users.noreply.github.com>
2022-03-26 15:32:29 -0400

    l

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
@@ -18,8 +19,19 @@ def index():
     f.write(str(count))
     f.close()
 
+    numb = random.randint(1,15)
+    win = """Congratulations, you've won the prize of a free trip to Los Santos all expenses paid"""
+    lose = """You didn't win any prize"""
+    sent = ""
+    if count == numb:
+        sent = win
+    else:
+        sent = lose    
+
+    
+
     # Render HTML with count variable
-    return render_template("index.html", count=count)
+    return render_template("index.html", count=count, sent=sent)
 
 if __name__ == "__main__":
     app.run()
