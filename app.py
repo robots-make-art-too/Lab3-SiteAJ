e3d9818d4e7022d58935c470b8fa95cad39857bd
ajkhaled <59937312+ajkhaled@users.noreply.github.com>
2022-03-31 04:14:17 -0400

    lab

--- app.py
+++ app.py
@@ -23,6 +23,41 @@ def index():
     win = """Congratulations, you've won the prize of a free trip to Los Santos all expenses paid"""
     lose = """You didn't win any prize"""
     sent = ""
+    webcount = ""
+
+    if count == 1:
+        webcount == "1st"
+    elif count == 2:
+        webcount == "2nd"
+    elif count == 3:
+        webcount == "3rd"
+    elif count == 4:
+        webcount == "4th"
+    elif count == 5:
+        webcount == "5th"
+    elif count == 6:
+        webcount == "6th"
+    elif count == 7:
+        webcount == "7th"
+    elif count == 8:
+        webcount == "8th"
+    elif count == 9:
+        webcount == "9th"
+    elif count == 10:
+        webcount == "10th"
+    elif count == 11:
+        webcount == "11th"
+    elif count == 12:
+        webcount == "12th"
+    elif count == 13:
+        webcount == "13th"
+    elif count == 14:
+        webcount == "14th"
+    else:
+        webcount == "15th"
+
+
+
     if count == numb:
         sent = win
     else:
@@ -31,7 +66,7 @@ def index():
     
 
     # Render HTML with count variable
-    return render_template("index.html", count=count, sent=sent)
+    return render_template("index.html", webcount=webcount, sent=sent)
 
 if __name__ == "__main__":
     app.run()
